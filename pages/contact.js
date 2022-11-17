c49ac19bc4c7ed106da1f26f0ce43748b8572680
rroell <roelliie@hotmail.nl>
2022-11-17 16:43:36 +0100

    Added contact page

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const contact = () => {
+  return (
+    <div>
+      <Meta title="Roel Duijsings - Contact" />
+      <h1>Contact details</h1>
+    </div>
+  );
+};
+
+export default contact;
