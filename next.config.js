1dbc48ae08064ef2cdee55c5342d8c859c16dd93
rroell <roelliie@hotmail.nl>
2022-11-26 16:13:01 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-//
-// module.exports = nextConfig
-
-// next.config.js
-//const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  // assetPrefix: isProd ? '/rroell.github.io/' : '',
-  // images: {
-  //   unoptimized: true,
-  // },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 };
+
+module.exports = nextConfig;
