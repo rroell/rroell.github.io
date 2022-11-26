edd9665fdef5c0d37dec8242013b96bbf57ece41
rroell <roelliie@hotmail.nl>
2022-11-26 15:54:55 +0100

    Add element key

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,11 +1,12 @@
 import ArticleItem from "./ArticleItem";
 import articleStyles from "../styles/Article.module.css";
+import { v4 as uuidv4 } from 'uuid';
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <ArticleItem article={article} />
+        <ArticleItem key={uuidv4} article={article} />
       ))}
     </div>
   );
