f46dd7e02fe6ae328a4b7891d29dc2ed8f17418c
rroell <roelliie@hotmail.nl>
2022-11-26 16:18:25 +0100

    Update index.js
    Change website name

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 // Choose what server to use: localhost:3000 when in development or websiteName else.
 const dev = process.env.NODE_ENV !== "production";
-const websiteName = "https://roelduijsings.nl";
+const websiteName = "https://rroell.github.io";
 export const server = dev ? "http://localhost:3000" : websiteName;
