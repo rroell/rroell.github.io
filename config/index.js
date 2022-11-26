cb1af24cbb1460f86757a1252585aea65d51a240
rroell <roelliie@hotmail.nl>
2022-11-26 18:42:47 +0100

    Change servername

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 // Choose what server to use: localhost:3000 when in development or websiteName else.
 const dev = process.env.NODE_ENV !== "production";
-const websiteName = "rroell.github.io";
+const websiteName =
+  "https://raw.githubusercontent.com/rroell/rroell.github.io/main";
 export const server = dev ? "http://localhost:3000" : websiteName;
