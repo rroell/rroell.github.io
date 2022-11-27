51bc753cc760950c11fe171436e8adc3e522c077
rroell <roelliie@hotmail.nl>
2022-11-27 20:49:03 +0100

    Icons

---
+++ components/HomeButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { IconButton } from "@mui/material";
+
+const HomeButton = () => {
+  return (
+    <IconButton>
+      <img src="/logo.png" width={150} height={50} />
+    </IconButton>
+  );
+};
+
+export default HomeButton;
