7e9e465848f6f4c28752fc0bc13ac68529e88569
rroell <roelliie@hotmail.nl>
2022-11-27 16:48:27 +0100

    Delete without articles

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,3 @@
-import Head from "next/head";
-import ArticleList from "../components/ArticleList";
-import { server } from "../config";
-
-export default function Home({ articles }) {
-  return (
-    <div>
-      <ArticleList articles={articles} />
-    </div>
-  );
+export default function Home() {
+  return <div>Hallo allemaal </div>;
 }
-
-// // get static articles from the data.js file using API
-// export const getStaticProps = async () => {
-//   const res = await fetch(
-//     `https://github.com/rroell/rroell.github.io/blob/main/data.js`
-//   );
-//   const articles = await res.json();
-//   return {
-//     props: { articles },
-//   };
-// };
-
-// get static articles from the internet as an example
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://github.com/rroell/rroell.github.io/blob/main/data.js`
-  );
-  const articles = await res.json();
-  return {
-    props: { articles },
-  };
-};
