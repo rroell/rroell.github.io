d423c9e7950149336582622b40492ab5d37526e8
rroell <roelliie@hotmail.nl>
2022-11-27 20:48:49 +0100

    Change navigation bar

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
-import Nav from "./Nav";
 import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 import Meta from "./Meta";
+import Navigation from "./Navigation";
 
 // general layout for all pages
 const Layout = ({ children }) => {
   return (
     <>
       <Meta />
-      <Nav />
+      <Navigation/>
       <div className={styles.container}>
         <main className={styles.main}>
           <Header />
